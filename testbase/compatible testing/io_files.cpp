a6fa6aff4c7d8d92b64265d809d48dc12a00af58
h114mx001 <tuananh17092003@gmail.com>
2020-08-25 15:21:48 +0700

    test commit

---
+++ testbase/compatible testing/io_files.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    freopen("task.inp","r",stdin);
+    freopen("task.out","w",stdout);
+    string n;
+    cin >> n;
+    cout << "This is " << n << ". Hello World!";
+}
