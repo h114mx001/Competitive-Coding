0107228101ddbb9e520b3af1516af28011bd04bf
h114mx001 <tuananh17092003@gmail.com>
2020-08-27 10:27:16 +0700

    brackets with small testcase

--- Algorithm/Branch_Bound/Exercise/Brackets/brackets.cpp
+++ Algorithm/Branch_Bound/Exercise/Brackets/brackets.cpp
@@ -4,14 +4,8 @@ int arr[100];
 int n;
 int level;
 string brackets = "()";
-void printResult(){
-    for (int i = 1; i <= n; i++){
-        cout << brackets[arr[i]];
-    }
-    cout << endl;
-}
 bool checkValid(){
-    int sum;
+    int sum = 0;
     for (int i = 1; i <= n; i++){
         if (arr[i] == 0){
             ++sum;
@@ -22,23 +16,31 @@ bool checkValid(){
 }
 int checkLevel(int j){
     int level = 1; 
+    int max = 0;
     for (int i = 1; i <= j - 1; i++){
         if (arr[i] == arr[i + 1]){
             ++level;
+            if (level > max) max = level;
         }
         else level = 1;
     }
-    return level;
+    return max;
+}
+void printResult(){
+    if (checkValid() && checkLevel(n) == level){
+        for (int i = 1; i <= n; i++){
+        cout << brackets[arr[i]];
+        }
+        cout << endl;
+    }
 }
 void Attempt(int i){
     for (int j = 0; j <= 1; j++){
         arr[i] = j;
-        if (checkLevel(i) <= level){
-            if (i == n - 1 && checkValid()){
-                printResult();
-            }
-            else Attempt(i+1);
+        if (i == n - 1){
+            printResult();
         }
+        else Attempt(i+1);
     }
 }
 int main(){
