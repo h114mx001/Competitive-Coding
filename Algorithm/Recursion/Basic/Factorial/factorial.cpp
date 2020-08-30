2041852ffcc995d6393b1b0da82e4975adf10b98
h114mx001 <tuananh17092003@gmail.com>
2020-08-30 10:33:15 +0700

    Recursion and some examples

---
+++ Algorithm/Recursion/Basic/Factorial/factorial.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int factorial(int n){
+    if (n == 0){
+        return 1;
+    }
+    else return factorial(n-1)*n;
+}
+int main(){
+    int n;
+    cin >> n; 
+    cout << factorial(3);
+}
