f421ac912966ce80993611273ce295b400c2abf8
h114mx001 <tuananh17092003@gmail.com>
2020-08-31 10:25:08 +0700

    Pointer and Struct

---
+++ ExtraInformation/Struct/Basic/struct.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct Student{
+    char name[30];
+    char fromClass[4];
+    int Math;
+    int Physic;
+    int Chemistry;
+    int Biology;
+    int English;
+};
+int main(){
+    struct Student NTA;
+    
+}
